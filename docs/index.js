b84bbbd0be03517f8685cb67eedeb79dcc3bb6ff
kem198 <kenkenpa198@gmail.com>
2025-11-01 14:48:49 +0900

    Implement localStorage functionality with event listeners for set, get, remove, and clear operations

--- docs/index.js
+++ docs/index.js
@@ -1,14 +1,33 @@
-console.log("Hello Tom and Jerry!");
+const TEST_KEY = "hello";
 
-localStorage.setItem("myCat", "Tom");
-localStorage.setItem("myMouse", "Jerry");
+const setLocalStorageButton = document.getElementById("setToLocalStorage");
+setLocalStorageButton.addEventListener("click", () => {
+  localStorage.setItem(TEST_KEY, "world");
+  console.log(`Set key "${TEST_KEY}" to localStorage.`);
+});
 
-const cat = localStorage.getItem("myCat");
-const mouse = localStorage.getItem("myMouse");
+const getLocalStorageButton = document.getElementById("getFromLocalStorage");
+getLocalStorageButton.addEventListener("click", () => {
+  const value = localStorage.getItem(TEST_KEY);
+  if (value) {
+    console.log(`Got value from localStorage. \n\nGot value: "${value}"`);
+  } else {
+    console.log(`Not founded key "${TEST_KEY}" in localStorage`);
+  }
+});
 
-console.log(cat);
-console.log(mouse);
+const removeLocalStorageButton = document.getElementById("removeLocalStorage");
+removeLocalStorageButton.addEventListener("click", () => {
+  if (localStorage.getItem(TEST_KEY)) {
+    localStorage.removeItem(TEST_KEY);
+    console.log(`Removed key: "${TEST_KEY}" from localStorage.`);
+  } else {
+    console.log(`Not founded key "${TEST_KEY}" in localStorage`);
+  }
+});
 
-// localStorage.removeItem("myCat");
-
-// localStorage.clear();
+const clearLocalStorageButton = document.getElementById("clearLocalStorage");
+clearLocalStorageButton.addEventListener("click", () => {
+  localStorage.clear();
+  console.log("Cleared localStorage.");
+});
