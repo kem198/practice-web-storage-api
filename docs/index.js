d66135c93da346f77d92735feba9bd66f9bf3b55
kem198 <kenkenpa198@gmail.com>
2025-11-01 12:11:28 +0900

    mod

--- docs/index.js
+++ docs/index.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+console.log("Hello Tom and Jerry!");
+
+localStorage.setItem("myCat", "Tom");
+localStorage.setItem("myMouse", "Jerry");
+
+const cat = localStorage.getItem("myCat");
+const mouse = localStorage.getItem("myMouse");
+
+console.log(cat);
+console.log(mouse);
+
+// localStorage.removeItem("myCat");
+
+// localStorage.clear();
